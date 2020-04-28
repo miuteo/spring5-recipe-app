258d19f1b8d5013bb0b24aef2832133b05d120c7
Teo Miu - Asus x556 <miu.teo@gmail.com>
2020-04-28 19:14:59 +0300

    spring data jpa query method

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,4 +1,26 @@
 package guru.springframework.controllers;
 
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @GetMapping("/")
+    public String getIndexPage(){
+        categoryRepository.findByDescription("Mexican")
+                .ifPresent(category -> System.out.println("Cat id is: "+category.getId()));
+        unitOfMeasureRepository.findByDescription("Pinch")
+                .ifPresent(unitOfMeasure -> System.out.println("UOM id is: "+unitOfMeasure.getId()));
+        return "index";
+    }
 }
